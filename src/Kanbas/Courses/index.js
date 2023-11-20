8bc8e99af597083bc23c5dac7242a84c6018251a
Gibran Myageri <myageri.g@northeastern.edu>
2023-11-20 16:28:52 -0500

    Corrected URL in courses
    Signed-off-by: Gibran <myageri.g@northeastern.edu>

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -12,11 +12,12 @@ import axios from "axios";
 
 function Courses() {
   const {courseId} = useParams();
-  const URL = "http://localhost:4000/api/courses";
+  const API_BASE = process.env.REACT_APP_API_BASE;
+  const COURSES_URL = `${API_BASE}/courses`;
   const [course, setCourse] = useState([]);
 
   const findCourseById = async (courseId) => {
-    const response = await axios.get(`${URL}/${courseId}`);
+    const response = await axios.get(`${COURSES_URL}/${courseId}`);
     setCourse(response.data);
   };
   useEffect(() => {
